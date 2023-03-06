43c14ce73d02f0076a20234969df8c565c7ddf63
Muhammad Tariq <tariqchemengr@gmail.com>
2023-03-06 23:38:20 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# piaic-websit
+ 
---
+++ tariq-piaic/src/app/page.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+  <>
+    <div>
+        <h1>PIAIC WEBSITE</h1>
+    </div>
+    <div>
+        <h1>Hello World!</h1>
+    </div>
+
+  </>
+    )
+}
