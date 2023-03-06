f72f8620522b94d8e15713f18522ad269a65cd63
Muhammad Tariq <tariqchemengr@gmail.com>
2023-03-07 00:44:27 +0500

    adding pages

---
+++ tariq-piaic/src/app/about/page.tsx
@@ -0,0 +1,12 @@
+
+export default function About(){
+    
+    return(
+        <>
+        <div>
+            about page
+        </div>
+        
+        </>
+    )
+}
---
+++ tariq-piaic/src/app/contactus/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Contact(){
+    
+    return(
+        <>
+        <div>
+            Contact us
+        </div>
+        
+        </>
+    )
+}
